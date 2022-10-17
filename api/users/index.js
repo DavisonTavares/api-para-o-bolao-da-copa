fb23b6e7a2a6019257dd5f30276188ec757d9734
DavisonTavares <99979943+DavisonTavares@users.noreply.github.com>
2022-10-16 23:10:19 -0300

    Adionado o update de pontos

--- api/users/index.js
+++ api/users/index.js
@@ -85,6 +85,14 @@ export const hunches = async ctx => {
       }
   }
   }
+  await prisma.user.update({
+    where: {
+      id: user.id
+    },
+    data:{
+      pontos
+    }
+  })
   ctx.body = {
       name: user.name,
       hunches,
