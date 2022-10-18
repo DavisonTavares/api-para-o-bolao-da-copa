d20c462b682b7c0eb989a90e6276e234e9d19cf6
DavisonTavares <99979943+DavisonTavares@users.noreply.github.com>
2022-10-18 12:27:26 -0300

    impedir data anterior

--- api/games/index.js
+++ api/games/index.js
@@ -6,6 +6,12 @@ let DiaAtual = new Date().toISOString('pt-BR')
 DiaAtual = getDayOfYear(new Date(DiaAtual))
 export const list = async ctx => {
   let currentDate = ctx.request.query.gameTime
+  if(currentDate < (new Date().toISOString('pt-BR'))){
+    return(
+      ctx.body = [],
+      ctx.status = 200
+    )
+  }
   const hora = getHours(new Date(currentDate))  
   const horaAtual = addHours(new Date(currentDate), -(hora + 3));
   if(getDayOfYear(new Date(currentDate)) > DiaAtual){
