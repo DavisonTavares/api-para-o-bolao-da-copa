5bf7fef0c9765eb2a8e664aa849c3087842760ac
DavisonTavares <99979943+DavisonTavares@users.noreply.github.com>
2022-10-18 13:41:56 -0300

    update game time e day

--- api/games/index.js
+++ api/games/index.js
@@ -6,7 +6,7 @@ let DiaAtual = new Date().toISOString('pt-BR')
 DiaAtual = getDayOfYear(new Date(DiaAtual))
 export const list = async ctx => {
   let currentDate = ctx.request.query.gameTime
-  if(currentDate < (new Date().toISOString('pt-BR'))){
+  if(getDayOfYear(new Date(currentDate)) < DiaAtual){
     return(
       ctx.body = [],
       ctx.status = 200
