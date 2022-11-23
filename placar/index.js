f1cfb3aa95b14264f85a896a6e33bc4b6788540a
DavisonTavares <99979943+DavisonTavares@users.noreply.github.com>
2022-11-23 19:13:54 -0300

    update

--- placar/index.js
+++ placar/index.js
@@ -39,7 +39,79 @@ export const placar = [
         id:'cl9erkqn90007vglc7q3ge1vp',
         homeTeam: 4,
         awayTeam: 1
+    },
+    {
+        id:'cl9erkqn90008vglcmo5rgtvl',
+        homeTeam: 0,
+        awayTeam: 0
+    },
+    {
+        id:'cl9erkqn90009vglcg1w9mfcx',
+        homeTeam: 1,
+        awayTeam: 2
+    },
+    {
+        id:'cl9erkqn9000avglc47ufg8gk',
+        homeTeam: 7,
+        awayTeam: 0
+    },
+    {
+        id:'cl9erkqn9000bvglc479awf9t',
+        homeTeam: 1,
+        awayTeam: 0
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
+    },
+    {
+        id:'cl9erkqn90007vglcddddddq3ge1vp',
+        homeTeam: -1,
+        awayTeam: -1
     }
+    
+    
 ]
 export default {
     placar,
